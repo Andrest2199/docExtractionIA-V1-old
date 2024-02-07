11651751b9f7a0830cfdab6d8c1d7c6c01a60590
Andrest2199 <128836304+Andrest2199@users.noreply.github.com>
2024-02-07 09:46:44 -0600

    AT-20240702-01
    [Data preprocessing and pre-trained models]
    
    Implementation of a preprocessing function to clean and fine-tune the data extracted from the images. Also, the creation of some pre-trained models that can be use to complement our OCR models.

---
+++ data_pre_proccessing/cleaning_extract_process.py
@@ -0,0 +1,120 @@
+# %% pre process data data%%
+import json
+import unicodedata
+import re
+
+def limpiar_json(json_str):
+    patron = re.compile(r'[^\w\s":{},]+')
+    json_str = patron.sub('', json_str)
+    json_str=json_str.strip()
+    decoded_json = json.loads(json_str)
+    
+    normalized_json = {key if unicodedata.is_normalized('NFKD',str(key)) else unicodedata.normalized('NFKD', str(key)): 
+                    value if unicodedata.is_normalized('NFKD', str(value)) else unicodedata.normalize('NFKD', str(value))
+                        for key, value in decoded_json.items()}
+    
+    return normalized_json
+
+def extraer_informacion(json_data):
+    #CAMPOS PRINCIPALES
+        #"extras"
+        #"folio"
+        #"posible_riesgo"
+        #"fecha_desde"
+        #"dias_incapacidad"
+        #"rama_incapacidad"
+        #"tipo_incapacidad" : #EG,MA,AT se obtiene de rama_incapacidad
+
+    campos_variaciones = [
+        #Extras
+        "nombre_asegurado",
+        "curp",
+        "serie_folio",
+        #Posible riesgo
+        "probable_riesgo_trabajo",
+        #Rama incapacidad
+        "ramo_seguro",
+        "ramo_de_seguro",
+        "Ramo de Seguro",
+        #Fecha desde
+        "a_partir_de",
+        "A partir del",
+        "inicio_incapacidad",
+        #Fecha desde en otro campo
+        "expedido_el",
+        "Expedido el",
+        #Dias Incapacidad
+        "numero",
+        "dias_autorizados_letra",
+        "numero_dias_autorizados",
+        "Dias Autorizados",
+        "direccion"
+        #Agregar más campos según sea necesario
+    ]
+    informacion_extraida = [campo.strip()+":"+json_data.get(campo,"").strip() if json_data.get(campo.strip()) else campo.strip()+":No se encontro" for campo in campos_variaciones]
+    
+    return informacion_extraida
+# %% Ejemplo de uso prueba%%
+json_str = '''
+{
+    "institucion": "INSTITUTO MEXICANO DEL SEGURO SOCIAL",
+    "direccion": "DIRECCI\u00d3N DE PRESTACIONES M\u00c9DICAS",
+    "documento": "CERTIFICADO DE INCAPACIDAD TEMPORAL PARA EL TRABAJO",
+    "nss": "9000-84-1206",
+    "agregado_medico": "1M19840R",
+    "nombre_asegurado": "ISIDRO $RAMOS CRUZ",
+    "curp": "RACI830115HCMRS00",
+    "sexo": "MASCULINO",
+    "delegacion": "OAXACA",
+    "unidad": {
+        "numero": "1",
+        "clave_presupuestal": "21012252110"
+    },
+    "consultorio": "5",
+    "turno": "MATUTINO",
+    "documento_identificacion_asegurado": "CREDENCIAL PARA VOTAR",
+    "numero_identificacion": "I270015670006",
+    "serie_folio": "WG667076",
+    "unidad_medica_expedidora": "1",
+    "nivel_atencion": "1",
+    "delegacion_adscripcion": "Oaxaca",
+    "tipo_incapacidad": "INICIAL",
+    "dias_autorizados": "uno",
+    "forma_seguro_enfermedad_general": "NO",
+    "probable_riesgo_trabajo": "NO",
+    "dias_acumulados": "0",
+    "particion": {
+        "expedidora": "Oaxaca",
+        "serie": "WG667076"
+    },
+    "patron": {
+        "nombre": "LEVI STRAUSS DE MEXICO",
+        "puesto_trabajo": "Demostradores y promotores"
+    },
+    "numero": "1",
+    "a_partir_de": "16/12/2023",
+    "expedido_el": "16/12/2023",
+    "control_maternidad": {
+        "no": "NA"
+    },
+    "nombre_firma_medico": "JAVIER ANTONIO L\u00d3PEZ AQUINO",
+    "matricula": "9921974-6",
+    "nombre_firma_medico_autoriza": "NO APLICA",
+    "matricula_autoriza": "NO APLICA",
+    "nota": "El asegurado a quien se entreg\u00f3 copia de este documento se encuentra incapacitado para trabajar a partir de la fecha y durante el periodo que se indica en este estudio.",
+    "informacion_adicional": "CONOCES EL SERVICIO DE CONSULTA DE INCAPACIDADES EN LINEA? Ingresa al escritorio virtual y podr\u00e1s revisar el historico de las incapacidades de los trabajadores de tu empresa. Si cuentas con Convenio de pago indirecto y reembolso de subsidios, tambi\u00e9n puedes descargar tus facturas de pago."
+}
+'''
+
+# %% Limpieza del JSON%%
+json_limpiado = limpiar_json(json_str)
+
+# %%Extracción de información%%
+informacion_extraida = extraer_informacion(json_limpiado)
+
+# print("JSON limpiado y con acentos convertidos:")
+# print(json.dumps(json_limpiado,ensure_ascii=False,indent=2,sort_keys=True))
+
+print("\nInformación extraída:")
+print(json.dumps(informacion_extraida,ensure_ascii=False,indent=2,sort_keys=True))
+# %%
