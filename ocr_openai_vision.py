3932409f3d991af4eb7a0c5c48874dc5e249eebd
Noé Rivera <noe@starlight.science>
2024-02-08 15:48:43 -0600

    fix openAI api key call

--- ocr_openai_vision.py
+++ ocr_openai_vision.py
@@ -7,7 +7,7 @@ import os
 from openai import OpenAI
 
 # OpenAI API Key
-OpenAI.api_key = os.environ["OPENAI_API_KEY_STARLIGHT"]
+OpenAI.api_key = os.environ["OPENAI_API_KEY"]
 
 # Get base path
 folder_base_path = os.getcwd()
@@ -24,9 +24,10 @@ TODO:
 
 # Define function to encode the image
 def encode_image(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
-  
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
 # Path to your image
 image_path = folder_base_path + "/1_image_procesed/0_procesed.jpeg"
 
@@ -43,31 +44,31 @@ client = OpenAI()
 
 # Get response
 response = client.chat.completions.create(
-  model="gpt-4-vision-preview",
-  messages=[
-    {
-      "role": "user",
-      "content": [
-        {"type": "text", "text": "Return JSON document with data. Only return JSON not other text."},
+    model="gpt-4-vision-preview",
+    messages=[
         {
-          "type": "image_url",
-          "image_url": {"url" : image_url}
-        },
-      ],
-    }
-  ],
-  max_tokens=2000
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Return JSON document with data. Only return JSON not other text.",
+                },
+                {"type": "image_url", "image_url": {"url": image_url}},
+            ],
+        }
+    ],
+    max_tokens=2000,
 )
 
 # Extract json content from response
 json_string = response.choices[0].message.content
-json_string = json_string.replace("```json\n","").replace("\n```","")
+json_string = json_string.replace("```json\n", "").replace("\n```", "")
 
 # Save json data
 json_data = json.loads(json_string)
-json_file_name = '0_text.json'
+json_file_name = "0_text.json"
 
-with open(folder_base_path + '/4_text_extracted/' + json_file_name, 'w') as file:
-  json.dump(json_data, file, indent=4)
+with open(folder_base_path + "/4_text_extracted/" + json_file_name, "w") as file:
+    json.dump(json_data, file, indent=4)
 
 # %%
