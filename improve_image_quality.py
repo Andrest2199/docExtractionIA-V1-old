437b444654ed32f39cc31bc1390c794adde35903
Noé Rivera <noe@starlight.science>
2024-02-09 14:11:06 -0600

    Add logit to detect skewnees, deskew and rotate the image, improve the 1bit grayscaling

--- improve_image_quality.py
+++ improve_image_quality.py
@@ -1,78 +1,91 @@
-from PIL import Image, ImageFilter, ImageEnhance
+from PIL import Image
+import numpy as np
+import cv2
+import math
+from typing import Tuple, Union
+from deskew import determine_skew
 
 
-def deskew_and_rotate(image):
+def get_skew_angle(cvImage) -> float:
+    """ "
+    Get the skew angle of the image using Hough Transform
+    return the average angle of the lines in the image
+    rtype: float
+    """
     # Convert the image to grayscale
+    gray = cv2.cvtColor(cvImage, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (9, 9), 0)
+    edges = cv2.Canny(blur, 50, 150, apertureSize=3)
+    lines = cv2.HoughLinesP(
+        edges, 1, np.pi / 180, 100, minLineLength=10, maxLineGap=250
+    )
+    angles = []
+    # calculate the angle of each line
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        angle = np.arctan2(y2 - y1, x2 - x1) * 180.0 / np.pi
+        angles.append(angle)
 
-    # Apply edge detection to find the skew angle
-    edges = image.filter(ImageFilter.FIND_EDGES)
-    skew_angle = -edges.rotate(45, expand=True).getbbox()[2] / 2
-
-    # Deskew the image
-    deskewed_image = image.rotate(skew_angle, expand=True)
-
-    # Rotate the image to make it upright
-    upright_image = deskewed_image.rotate(90 - skew_angle, expand=True)
-
-    return upright_image
-
-
-def background_removal(image):
-    # Convert the image to grayscale
-    # grayscale_image = make_grayscale(image)
-    # Apply thresholding to create a binary image
-    threshold = 128
-    binary_image = image.point(lambda p: p > threshold and 255)
-
-    # Perform image segmentation to separate text from background
-    segmented_image = binary_image.filter(ImageFilter.FIND_EDGES)
-    segmented_image = segmented_image.filter(ImageFilter.SMOOTH_MORE)
-
-    return segmented_image
+    # return the average angle
+    return np.mean(angles)
 
 
-def make_grayscale(image):
-    # Convert the image to grayscale
-    grayscale_image = image.convert("L")
-    return grayscale_image
+def deskew_and_rotate(image, output_path):
+    """ "
+    Deskew and rotate the image
+    rtpe: PIL.Image.Image
+    """
+    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    angle = determine_skew(grayscale)
+    rotated = rotate(image, angle, (0, 0, 0))
+    cv2.imwrite(output_path, rotated)
+    output_image = Image.open(output_path)
+    return output_image
 
 
 def convert_to_1bit(image):
-    # Convert the image to 1-bit (black and white)
-    binary_image = image.convert("1")
+    """ "
+    Convert the image to 1-bit (black and white)
+    rtype: PIL.Image.Image
+    """
+    np_image = np.array(image)
+    np_image = cv2.cvtColor(np_image, cv2.COLOR_BGR2GRAY)
+    binary_image = cv2.adaptiveThreshold(
+        np_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+    )  # imgf contains Binary image
+
     return binary_image
 
+def rotate(
+    image: np.ndarray, angle: float, background: Union[int, Tuple[int, int, int]]
+) -> np.ndarray:
+    old_width, old_height = image.shape[:2]
+    angle_radian = math.radians(angle)
+    width = abs(np.sin(angle_radian) * old_height) + abs(
+        np.cos(angle_radian) * old_width
+    )
+    height = abs(np.sin(angle_radian) * old_width) + abs(
+        np.cos(angle_radian) * old_height
+    )
 
-def resize_image(image):
-    # Calculate the desired DPI
-    dpi = 400
-
-    # Calculate the new size based on the desired DPI
-    width, height = image.size
-    new_width = int((width / 72) * dpi)
-    new_height = int((height / 72) * dpi)
-
-    # Resize the image to the new size
-    resized_image = image.resize((new_width, new_height), Image.LANCZOS)
-
-    # Set the DPI metadata of the image
-    resized_image.info["dpi"] = (dpi, dpi)
-
-    return resized_image
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    rot_mat[1, 2] += (width - old_width) / 2
+    rot_mat[0, 2] += (height - old_height) / 2
+    return cv2.warpAffine(
+        image, rot_mat, (int(round(height)), int(round(width))), borderValue=background
+    )
 
 
 def improve_image_quality(input_image_path, output_image_path):
-    # Open the image file
-    image = Image.open(input_image_path).convert("RGB")
 
-    # Deskew and rotate the image
-    # processed_image = deskew_and_rotate(grayscale_image)
-    processed_image = resize_image(image)
-
-    enhancer = ImageEnhance.Brightness(processed_image)
-    # Apply the enhancement
-    enhanced_image = enhancer.enhance(1.5)
-    enhanced_image = make_grayscale(enhanced_image)
-
-    # Save the enhanced image
-    enhanced_image.save(output_image_path)
+    raw_image = cv2.imread(input_image_path)
+    skewed_angle = get_skew_angle(raw_image)
+    if abs(skewed_angle) > 0.5:
+        print(f"Image {input_image_path} is skewed by {skewed_angle} degrees")
+        deskewed_image = deskew_and_rotate(raw_image, output_image_path)
+    else:
+        deskewed_image = raw_image
+    bin_img = convert_to_1bit(deskewed_image)
+    cv2.imwrite(output_image_path, bin_img)
+    print(f"Image {input_image_path} improved and saved to {output_image_path}")
