1518f11de8dc88efad26af9124186e46826f2b64
Noé Rivera <noe@starlight.science>
2024-02-13 20:51:42 -0600

    abstracts serialization

--- utils.py
+++ utils.py
@@ -5,12 +5,13 @@ def get_all_file_paths(folder_path):
     """
     Get all file paths in a folder, ignoring .DS_Store and .gitignore files
     """
-    file_names = []
+    file_paths = []
     for root, dirs, files in os.walk(folder_path):
         for file in files:
-            if not file.endswith((".DS_Store", ".gitignore")):
-                file_names.append(file)
-    return file_names
+            if ".DS_Store" not in file and ".gitignore" not in file:
+                file_path = os.path.join(root, file)
+                file_paths.append(file_path)
+    return file_paths
 
 
 def create_file_list(folder_path):
@@ -22,7 +23,8 @@ def create_file_list(folder_path):
             "ascii", errors="ignore"
         ).decode()
         # Append original file name cleaned
-        file_name_list.append(file_name_original_clean)
+        if not file_name_original.endswith((".DS_Store", ".gitignore")):
+            file_name_list.append(file_name_original_clean)
     return file_name_list
 
 
