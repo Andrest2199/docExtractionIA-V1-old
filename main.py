a08788a57b4276a76e0697d1c3fdfbe693fb83e9
Noé Rivera <noe@starlight.science>
2024-02-14 16:04:49 -0600

    invokes retrieval function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from image_pre_procesing import process_images
 from improve_image_quality import improve_image_quality
 from ocr_openai_vision import ocr_openai_vision
 from ocr_google_vision import ocr_google_vision
+from cleaning_extract_process import data_retrieval
 import os
 import utils
 
@@ -12,6 +13,7 @@ image_raw_folder = folder_base_path + "/0_image_raw"
 image_preprocessed_folder = folder_base_path + "/1_image_preprocessed"
 image_improved_folder = folder_base_path + "/2_image_improved"
 text_extracted_folder = folder_base_path + "/3_text_extracted"
+results_folder = folder_base_path + "/4_results"
 
 
 # %% Main function
@@ -34,6 +36,14 @@ def main():
     # %% ocr_google_vision
     for image in improved_images:
         ocr_google_vision(image_improved_folder + "/" + image, text_extracted_folder)
+
+    # %% Extract data
+    all_text_files = utils.list_text_files(text_extracted_folder)
+    for text_file in all_text_files:
+        data_retrieval(
+            text_extracted_folder + "/" + text_file,
+            results_folder,
+        )
 
 
 # %% Run main function
