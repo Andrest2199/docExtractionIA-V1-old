304f14f63f5ad1b99c15f6160e5cc3d2743936a2
Noé Rivera <noe@starlight.science>
2024-02-22 13:48:01 -0600

    Modify class for file handling

--- file_utils.py
+++ file_utils.py
@@ -25,7 +25,7 @@ class FileUtils:
         for file_name_original in os.listdir(folder_path):
             # Remove non-ASCII characters
             file_name_original_clean = file_name_original.encode(
-                "ascii", errors="ignore"
+                # "ascii", errors="ignore"
             ).decode()
             # Append original file name cleaned
             if not file_name_original.endswith((".DS_Store", ".gitignore", ".json")):
@@ -40,6 +40,7 @@ class FileUtils:
                 file_name_list.append(file)
         return file_name_list
 
+    @staticmethod
     def delete_from_folder(folder_path):
         for filename in os.listdir(folder_path):
             file_path = os.path.join(folder_path, filename)
@@ -51,13 +52,21 @@ class FileUtils:
             except Exception as e:
                 print(f"Failed to delete {file_path}. Reason: {e}")
 
-    def save(file_path_output, str):
-        with open(file_path_output, "w") as file:
-            file.write(str)
+    @staticmethod
+    def save(file_path_output, data):
+        if isinstance(data, str):
+            with open(file_path_output, "w") as file:
+                file.write(data)
+        else:
+            with open(file_path_output, "wb") as file:
+                file.write(data)
 
+    @staticmethod
     def get_original_names(input_folder_path):
-        file_name_list = []
-        for file_name_original in os.listdir(input_folder_path):
-            if file_name_original != ".gitignore" and file_name_original != ".DS_Store":
-                file_name_list.append(file_name_original)
-        return file_name_list
+        # Create data dictionary and extract original file name
+        file_name_list = FileUtils.create_list(input_folder_path)
+        data = {}
+        for ii, file_name_original in enumerate(file_name_list):
+            if file_name_original not in [".gitignore", ".DS_Store"]:
+                data[str(ii)] = {"file_name_original": file_name_original}
+        return data
