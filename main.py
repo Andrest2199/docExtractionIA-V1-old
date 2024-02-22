53872ec77a44a361480ec8c2b0a6ad62bac68dd1
Noé Rivera <noe@starlight.science>
2024-02-22 13:48:37 -0600

    add call to functions in image preprocessing and image identifying

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 # %% Import libraries
 import json
-from image_pre_procesing import process_images
+from image_pre_procesing import (
+    process_images,
+    identify_file,
+    get_images_from_pdf,
+    pdf_has_text,
+    get_text_from_pdf,
+)
 from improve_image_quality import improve_image_quality
 from ocr_openai_vision import ocr_openai_vision
 from ocr_google_vision import ocr_google_vision
 from cleaning_extract_process import data_cleaning, data_extraction, regex_extraction
 import os
-import utils
+from file_utils import FileUtils
 import os
 
 # %% Set the paths
+
 folder_base_path = os.getcwd()
 image_raw_folder = os.path.join(folder_base_path, "0_image_raw")
 image_preprocessed_folder = os.path.join(folder_base_path, "1_image_preprocessed")
@@ -19,7 +26,32 @@ results_folder = os.path.join(folder_base_path, "4_results")
 
 
 # %% Main function
-def main():
+def main(file_path=str, doctype=str) -> dict:
+    if doctype not in ["IMSS", "ISSTE", "SAT"]:
+        raise ValueError(
+            "Document type not recognized. please provide a document type: IMSS, ISSTE, SAT"
+        )
+    if doctype == "IMSS":
+        file_name = os.path.basename(file_path)
+        filetype = identify_file(file_name)
+        if filetype == "pdf":
+            has_text = pdf_has_text(file_path)
+            if has_text:
+                print("file has text inside")
+                text_corpus = get_text_from_pdf(file_path)
+                # new_file_name = file_name.split("/")[-1].strip(".pdf") + ".txt"
+                new_file_name = file_name.strip(".pdf") + ".txt"
+                FileUtils.save(text_extracted_folder + "/" + new_file_name, text_corpus)
+
+            else:
+                print("file does not have text inside")
+                get_images_from_pdf(file_path, image_preprocessed_folder)
+        else:
+            print("file is not a pdf")
+            process_images(file_path, image_preprocessed_folder)
+        # process_images(file, image_preprocessed_folder)
+    #  %% improves image quality
+    """Main function to process the images and extract the data
     # Process images
     process_images(image_raw_folder, image_preprocessed_folder)
     #  %% improves image quality
@@ -56,10 +88,30 @@ def main():
             extracted_text = data_extraction(
                 data_cleaned, "operaciones", "incapacidades"
             )
+    """
+    # return extracted_json
 
 
 # %% Run main function
 if __name__ == "__main__":
-    main()
+    # Remove files for all output folders
+    FileUtils.delete_from_folder(image_preprocessed_folder)
+    FileUtils.delete_from_folder(image_improved_folder)
+    FileUtils.delete_from_folder(text_extracted_folder)
+    FileUtils.delete_from_folder(results_folder)
+    # get the list of files unprocessed for IMSS
+    raw_imss_list = FileUtils.create_list(os.path.join(image_raw_folder, "IMSS"))
+    # get the file paths for every image inside imss folder
+    imss_files_path = os.path.join(image_raw_folder, "IMSS")
+    # get the original names of the files
+    imss_original_names = FileUtils.get_original_names(imss_files_path)
+    # save the original names of the files
+    # FileUtils.save(
+    #     os.path.join(image_preprocessed_folder, "imss_original_filenames.json"),
+    #     json.dumps(imss_original_names),
+    # )
+    for file in raw_imss_list:
+        file = os.path.join(imss_files_path, file)
+        main(file, "IMSS")
 
 # %%
