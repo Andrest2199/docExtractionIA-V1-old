86b985d7b0f68bed3dae3a3bcd49e9c28a1391ea
Noé Rivera <noe@starlight.science>
2024-04-07 22:22:02 -0600

    Add main recognition view

--- ono_ocr/views.py
+++ ono_ocr/views.py
@@ -1,9 +1,28 @@
-from django.http import HttpResponse
+import json
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from ono_ocr.recognition_worker import recognition_worker
 
 
 # Create your views here.
 def ocr_recognize(request):
-    # Your code here
-    
-    return HttpResponse("Hello, World! from the recognize function in the views.py file.")
+    if request.method != "POST":
+        return JsonResponse("Method not allowed", status=405)
+
+    try:
+        req = json.loads(request.body.decode("utf-8"))
+        doctype = req.get("doctype")
+        document_path = req.get("document_path")
+        data = recognition_worker(document_path, doctype)
+
+        return JsonResponse(
+            {
+                "message": "Success",
+                "doc_path": document_path,
+                "doctype": doctype,
+                "data": data,
+            },
+            status=200,
+        )
+    except Exception as e:
+        return JsonResponse(str(e), status=500, safe=False)
