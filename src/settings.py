be65bfd691971fe11671b482f133aa27b9711d0f
Noé Rivera <noe@starlight.science>
2024-04-16 01:31:24 -0600

    add static files and removes requirement

--- src/settings.py
+++ src/settings.py
@@ -33,7 +33,7 @@ DEBUG = env("DEBUG")
 if DEBUG:
     ALLOWED_HOSTS = ["127.0.0.1"]
 else:
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['44.197.174.193']
 
 
 # Application definition
@@ -133,7 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
