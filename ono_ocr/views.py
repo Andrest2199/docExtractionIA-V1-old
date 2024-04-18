41b73c594eb65cb1f6446b1dc911b71bb3c1d00b
Noé Rivera <noe@starlight.science>
2024-04-18 11:02:09 -0600

    add static files

--- ono_ocr/views.py
+++ ono_ocr/views.py
@@ -5,10 +5,11 @@ from ono_ocr.recognition_worker import recognition_worker
 from django.views.decorators.csrf import csrf_exempt
 from django.template import loader
 
+
 @csrf_exempt
 def index(request):
-    template = loader.get_template('ono_ocr/views/index.html')
-    return HttpResponse(template.render({}, request))
+    return render(request, "index.html")
+
 
 # Create your views here.
 @csrf_exempt
@@ -21,13 +22,10 @@ def ocr_recognize(request):
         filename = req.get("filename")
         doctype = req.get("doctype")
         file_base64 = req.get("file_base64")
+        if filename is None or doctype is None or file_base64 is None:
+            return JsonResponse( f'Missing parameters {request.body.decode("utf-8")}',  status=400, safe=False)
         data = recognition_worker(filename, doctype, file_base64)
+        return JsonResponse(data, status=200, safe=False)
 
-        return JsonResponse(
-            data,
-            status=200,
-            safe=False
-        )
     except Exception as e:
         return JsonResponse(str(e), status=500, safe=False)
-    
