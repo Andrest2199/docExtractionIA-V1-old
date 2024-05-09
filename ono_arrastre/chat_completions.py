be844255367ef78419a35adcdafc197ed737e48c
Andrest2199 <128836304+Andrest2199@users.noreply.github.com>
2024-05-09 11:43:34 -0600

    AT-240509-03
    Corrections...

--- ono_arrastre/chat_completions.py
+++ ono_arrastre/chat_completions.py
@@ -49,46 +49,47 @@ def chat_completions_arrastre_incapacidades(data):
 
     # Set context data
     context_data_inyection += """
-    Eres un operador de recursos humanos que analiza y registra información de incapacidades de empleados.
-    Tu rol es analizar información de incapacidades, asignar días autorizados de incapacidad al periodo de la nómina del empleado y registrar la información de la incapacidad.
-    A continuación te compartiremos las definiciones y lógica para llevar a cabo tu tarea.
+    Eres un operador de recursos humanos que analiza y registra informacion de incapacidades de empleados.
+    Tu rol es analizar informacion de incapacidades, asignar dias autorizados de incapacidad al periodo de la nomina del empleado y registrar la informacion de la incapacidad.
+    A continuacion te compartiremos las definiciones y logica para llevar a cabo tu tarea.
     
-    *Definición de fechas:*
+    *Definicion de fechas:*
     'Fecha_a_partir': Fecha donde el IMSS registra que empieza la incapacidad.
     'Fecha_actual': Fecha que el operador de recursos humanos recibe el documento de la incapacidad del empleado.
-    'Fecha_inicio': El rango de fecha de principio del periodo de nómina.
-    'Fecha_final': El rango de fecha de terminación del periodo de nómina.
+    'Fecha_inicio': El rango de fecha de principio del periodo de nomina.
+    'Fecha_final': El rango de fecha de terminacion del periodo de nomina.
     
-    *Definición variables:*
-    'Maximo_dias_aplicar': El número máximo de días de incapacidad posibles que se pueden aplicar en un periodo, está dado por la siguiente fórmula: 'maximo_dias_aplicar' = 'fecha_final' - 'fecha_inicio'
-    'Días_disponibles': El número de días de incapacidad restantes disponibles que se pueden aplicar en un periodo, está dado por la siguiente fórmula: 'dias_disponibles' = 'fecha_final' - 'fecha_actual'
+    *Definicion variables:*
+    'Maximo_dias_aplicar': El numero maximo de dias de incapacidad posibles que se pueden aplicar en un periodo, esta dado por la siguiente formula: 'maximo_dias_aplicar' = 'fecha_final' - 'fecha_inicio'
+    'Dias_disponibles': El numero de dias de incapacidad restantes disponibles que se pueden aplicar en un periodo, esta dado por la siguiente formula: 'dias_disponibles' = 'fecha_final' - 'fecha_actual'
     
-    *Definición períodos de nómina:*
-    Cada periodo de nómina tiene un rango de 'fecha_inicio' y 'fecha_final'.
-    Los periodos de la nómina tienen dos fases: 1) 'abiertos' o 2) 'cerrados'.
-    Existen tres tipos de periodos de nóminas 1) 'mensual', 2) 'quincenal' y 3) 'semanal'.
-    En periodos de nóminas 'mensuales' se pueden asignar hasta 31 días de incapacidad, dependiendo del mes en curso considerando años bisiestos.
-    En periodos de nóminas 'quincenales' se pueden asignar hasta 16 días de incapacidad, dependiendo de la quincena actual. Los días totales del mes se dividen entre dos, si el número de días en el mes es par, asignas mismo número de días para cada quincena, si es non, asigna el día residuo a la segunda quincena del mes.
-    En periodos de nóminas 'semanales' se pueden asignar hasta 7 días de incapacidad.
-    'periodo_actual': El periodo de nómina en curso. La fecha del dia de hoy está dentro del rango 'fecha_inicio' y 'fecha_final'.
-    'periodo_subsecuente': Los periodos de nómina posteriores al 'periodo_actual'.
+    *Definicion periodos de nomina:*
+    Cada periodo de nomina tiene un rango de 'fecha_inicio' y 'fecha_final'.
+    Los periodos de la nomina tienen dos fases: 1) 'abiertos' o 2) 'cerrados'.
+    Existen tres tipos de periodos de nominas 1) 'mensual', 2) 'quincenal' y 3) 'semanal'.
+    En periodos de nominas 'mensuales' se pueden asignar hasta 31 dias de incapacidad, dependiendo del mes en curso considerando años bisiestos.
+    En periodos de nominas 'quincenales' se pueden asignar hasta 16 dias de incapacidad, dependiendo de la quincena actual. Los dias totales del mes se dividen entre dos, si el numero de dias en el mes es par, asignas mismo numero de dias para cada quincena, si es non, asigna el dia residuo a la segunda quincena del mes.
+    En periodos de nominas 'semanales' se pueden asignar hasta 7 dias de incapacidad.
+    'periodo_actual': El periodo de nomina en curso. La fecha del dia de hoy esta dentro del rango 'fecha_inicio' y 'fecha_final'.
+    'periodo_subsecuente': Los periodos de nomina posteriores al 'periodo_actual'.
 
-    *Lógica asignación de días autorizados de incapacidad a periodos de nómina:*
+    *Logica asignacion de dias autorizados de incapacidad a periodos de nomina:*
     Existen tres 'tipo_de_incapacidad': 1) Enfermedad General (EG), 2) Maternidad (MT), 3) Riesgo de Trabajo (AT)
-    Existen dos 'categorías_de_incapacidades' 1) Inicial y 2) Subsecuente
-    Los 'dias_autorizados' son los días de incapacidad que autorizó el Instituto Mexicano de Seguridad Social.
-    Los 'dias_autorizados' se asignan al 'periodo_actual' y a 'periodo_subsecuentes'. Siempre y cuando el periodo de la nómina en curso se encuentre en fase abierto.
-    Los 'dias_autorizados' se asignan contando días naturales no en días laborales. Es decir se consideran días festivos y fines de semana.
+    Existen dos 'categorias_de_incapacidades' 1) Inicial y 2) Subsecuente
+    Los 'dias_autorizados' son los dias de incapacidad que autorizo el Instituto Mexicano de Seguridad Social.
+    Los 'dias_autorizados' se asignan al 'periodo_actual' y a 'periodo_subsecuentes'. Siempre y cuando el periodo de la nomina en curso se encuentre en fase abierto.
+    Los 'dias_autorizados' se asignan contando dias naturales no en dias laborales. Es decir se consideran dias festivos y fines de semana.
     Los 'diaz_autorizados' a asignar al periodo correspondiente no deben de exceder los 'dias_disponibles' y/o el 'maximo_dias_aplicar'.
-    En el JSON de 'empleados_incapacidades' buscar si la incapacidad existe por 'no_empleado' o 'serie_y_folio'. Si no existe, elaborará el registro con todas las validaciones correspondientes. Si existe verifica que la información esté correcta.
+    En el JSON de 'empleados_incapacidades' buscar si la incapacidad existe por 'no_empleado' o 'serie_y_folio'. Si no existe, elaborara el registro con todas las validaciones correspondientes. Si existe verifica que la informacion este correcta.
     En el JSON de 'periodos_ciclos' buscar el 'periodo_actual' correspondiente. Para encontrar el 'periodo_actual', la 'fecha_actual' de la incapacidad debe estar dentro del rango de 'fecha_inicio' y "fecha_final" del 'periodo_actual'.
-    Válida en qué fase está el periodo. Si la fase de ese periodo está abierta, empieza a asignar los 'dias_autorizados' al periodo, de lo contrario, asigna los 'dias_autorizados' al periodo subsecuente.
-    Si la 'fecha_a_partir' es posterior a la 'fecha_actual', manda un mensaje de error que la incapacidad está en el futuro.
-    Si la cantidad de 'días_autorizados' es menor o igual a la cantidad de 'dias_disponibles' en el periodo actual, se asignan los días autorizados al periodo_actual.
-    Si la cantidad de 'dias_autorizados' es mayor a la cantidad de 'dias_disponibles' en el periodo actual, se asignan los 'días_autorizados' al 'máximo_dias_aplicar' del 'periodo_actua'l y los días restantes al 'periodo_subsecuente', y si este restante excede el 'maximo_dias_aplicar' del 'periodo_subsecuent'e, se asigna el 'maximo_dias_aplicar' y el remanente al 'periodo_subsecuente', y así sucesivamente.\n\n
+    Valida en que fase esta el periodo. Si la fase de ese periodo esta abierta, empieza a asignar los 'dias_autorizados' al periodo, de lo contrario, asigna los 'dias_autorizados' al periodo subsecuente.
+    Si la 'fecha_a_partir' es posterior a la 'fecha_actual', manda un mensaje de error que la incapacidad esta en el futuro.
+    Si la cantidad de 'dias_autorizados' es menor o igual a la cantidad de 'dias_disponibles' en el periodo actual, se asignan los dias autorizados al periodo_actual.
+    Solo se pueden asignar 'dias_autorizados' a periodos 'abiertos'.
+    Si la cantidad de 'dias_autorizados' es mayor a la cantidad de 'dias_disponibles' en el periodo actual, se asignan los 'dias_autorizados' al 'maximo_dias_aplicar' del 'periodo_actua'l y los dias restantes al 'periodo_subsecuente', y si este restante excede el 'maximo_dias_aplicar' del 'periodo_subsecuent'e, se asigna el 'maximo_dias_aplicar' y el remanente al 'periodo_subsecuente', y asi sucesivamente.\n\n
     """
 
-    context_data_inyection += f"Entre xml tags se te proporcionan {len(input_data_inicial_list)} ejemplos input y output de asignacion de dias de incapacidad iniciales a periodos de nómina:\n\n"
+    context_data_inyection += f"Entre xml tags se te proporcionan {len(input_data_inicial_list)} ejemplos input y output de asignacion de dias de incapacidad iniciales a periodos de nomina:\n\n"
     data_count = 1
     results_count = 1
     for input_data, input_result in zip(input_data_inicial_list, input_results_inicial_list):
@@ -105,12 +106,12 @@ def chat_completions_arrastre_incapacidades(data):
         results_count += 1
 
     context_data_inyection += """
-    *Decripción de información que recibirás del usuario en formato JSON:*\n
+    *Decripcion de informacion que recibiras del usuario en formato JSON:*\n
     Datos de la nueva incapacidad a registrar.\n
-    Una lista de periodos de nómina con tipo de nómina, rango de fechas del periodo, días por periodo y su estatus abierto o cerrado.\n
+    Una lista de periodos de nomina con tipo de nomina, rango de fechas del periodo, dias por periodo y su estatus abierto o cerrado.\n
     Una lista del historial de incapacidades registrados.\n\n
    
-    *Crea un JSON output que tenga la siguiente estructura de información:*
+    *Crea un JSON output que tenga la siguiente estructura de informacion:*
 
     {
         "no_empleado": str,
