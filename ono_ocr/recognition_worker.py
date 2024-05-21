b38a4f09dca3de3cc0d34a732c892943afaf9ec1
Noé Rivera <noe@starlight.science>
2024-05-20 20:17:29 -0600

    get thresholds from worker

--- ono_ocr/recognition_worker.py
+++ ono_ocr/recognition_worker.py
@@ -1,5 +1,7 @@
 import os
 from ono_ocr.models import Extraction
+from ono_ocr.thresholds import IMSS_MEAN_LENGTH, IMSS_STD_LENGTH, INFONAVIT_MEAN_LENGTH, INFONAVIT_STD_LENGTH, \
+    SAT_MEAN_LENGTH, SAT_STD_LENGTH
 from ono_ocr.utils import FileUtils, Utils
 from ono_ocr.document_handler import document_handler
 from ono_ocr.entity_recognition import chat_completions_entity_extraction
@@ -17,6 +19,7 @@ image_preprocessed_folder = os.path.join(base_dir, "1_image_preprocessed")
 # text_extracted_folder = os.path.join(base_dir, "3_text_extracted")
 # results_folder = os.path.join(base_dir, "4_results")
 data_inject_folder = os.path.join(base_dir, "data_inject")
+
 
 # TODO: Add logic for two paged documents
 # TODO: return instructions in the api (system_role and user content)
@@ -49,6 +52,11 @@ def recognition_worker(filename=str, doctype=str, file_base64=str) -> dict:
         # file_path = image_raw_folder + "/" + os.path.basename(file_path)
         text_extracted = document_handler(file_path, doctype)
 
+        # Check if the text extracted is of good quality
+        text_quality = raw_text_validator(text_extracted, doctype)
+        if text_quality[0] is False:
+            return {"error": text_quality[1]}
+
         fields_extracted = chat_completions_entity_extraction(
             text_extracted, data_inject_folder, doctype
         )
@@ -68,3 +76,22 @@ def recognition_worker(filename=str, doctype=str, file_base64=str) -> dict:
     except Exception as e:
         traceback.print_exc()
         return {"error": str(e)}
+
+
+def raw_text_validator(text_extracted, doctype):
+    """
+    This function validates the raw text extracted from the documents.
+    """
+    if len(text_extracted) < 10:
+        return False, "Text extracted is too short. Please try again with a better quality image."
+
+    doc_thresholds = {
+        "IMSS": IMSS_MEAN_LENGTH - IMSS_STD_LENGTH,
+        "INFONAVIT": INFONAVIT_MEAN_LENGTH - INFONAVIT_STD_LENGTH,
+        "SAT": SAT_MEAN_LENGTH - SAT_STD_LENGTH,
+    }
+
+    if doctype in doc_thresholds and len(text_extracted) < doc_thresholds[doctype]:
+        return False, "Text extracted is too short. Please try again with a better quality image."
+
+    return True, "Text length is within acceptable range."
